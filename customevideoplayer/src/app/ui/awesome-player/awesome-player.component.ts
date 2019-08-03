f87d40fc99213a22379a78425ace3b97f187c2e0
unknown <mln.prvn@gmail.com>
2019-08-03 12:41:31 +0530

    changes to include featurestate

--- customevideoplayer/src/app/ui/awesome-player/awesome-player.component.ts
+++ customevideoplayer/src/app/ui/awesome-player/awesome-player.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ElementRef , ViewChild, AfterViewInit} from '@angular/core';
 import { PlayerComponent } from 'src/app/ui/player/player.component';
 import { ControlsComponent } from 'src/app/ui/controls/controls.component';
+import { FeatureStateService, FeaturestateService } from  'src/app/service/featurestate.service';
 
 @Component({
   selector: 'app-awesome-player',
@@ -13,15 +14,24 @@ export class AwesomePlayerComponent implements OnInit,AfterViewInit  {
   @ViewChild(PlayerComponent) playerRef: PlayerComponent;
   @ViewChild(ControlsComponent) controlsRef: ControlsComponent;
   private toggle = false;
-  constructor() { }
+  private featureState;
+  private awesomePlayer;
+  constructor(featureStateService:FeaturestateService) { 
+   this.featureState = featureStateService;
+   this.awesomePlayer = this.featureState.get("awesomeplayer") ;
+   this.awesomePlayer.playlist = [];
+   this.awesomePlayer.selectedPlaylist = {};
+  }
 
   ngOnInit() {
+    
+    AwesomePlayer
   }
   ngAfterViewInit() {
-    console.log(this.controlsRef);
+    //console.log(this.controlsRef);
     this.controlsRef.playerRef(this.playerRef.getplayerInstance());
-    console.log(this.playerRef.getplayerInstance());
-    console.log('player instance \n '+this.controlsRef.player);
+    //console.log(this.playerRef.getplayerInstance());
+    //console.log('player instance \n '+this.controlsRef.player);
   }
   public togglePotraitMode() {
    this.toggle = !this.toggle  ;
